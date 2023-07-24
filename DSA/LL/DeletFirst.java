69c7acd55f2180dde7cd640ed388c9713f2225bc
Saagar02 <sagarchaudhari2202@gmail.com>
2023-07-24 07:27:30 +0530

    multi

---
+++ DSA/LL/DeletFirst.java
@@ -0,0 +1,108 @@
+import java.io.*;
+class LL{
+	Node  head;
+	class Node{
+		String data;
+		Node next;
+
+		public Node(String data){
+			this.data = data;
+			this.next = null;	
+		}
+
+	}
+
+	public Node createNode(){
+		System.out.print("Enter the data : ");
+		String data = null;
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			data= br.readLine();
+		}catch(IOException e){
+		
+		}
+		
+		Node newNode = new Node(data);
+		return newNode;
+	}
+	public void addFirst(){
+
+		Node newNode = createNode();
+		if(head  == null){
+			head = newNode;
+			return;	
+		}
+
+		newNode.next = head;
+		head = newNode;
+	
+	}
+	public void deletFirst(){
+		
+		if(head == null){
+			System.out.println("Invalid operation empty LinkList");
+			return ;
+		}
+
+		head = head.next;
+	}
+		
+	public void addLast(){
+	
+		Node newNode = createNode();
+		if(head == null){
+			head = newNode;
+			return;
+		}
+
+		Node temp = head;
+
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		temp.next = newNode;
+	}
+
+	public void  printll(){
+		if(head == null){
+			System.out.println("Empty LL");
+			return;
+		}
+		Node temp = head;
+		while(temp.next != null){
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.print(temp.data+"\n");
+	
+	}
+	public static void main(String[] args) throws IOException{
+		LL list = new LL();
+		char ans = 'y';
+		while(ans != 'n' || ans != 'N'){
+			System.out.println("Enter your choice : \n 1.Add Node at first \n 2.Add Node at last \n 3.DeletFirst\n 6.Print ll");
+			int choice = 0;
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			choice = Integer.parseInt(br.readLine());
+		 	switch(choice){
+	 			case 1 :
+					list.addFirst();
+					break;
+				case 2 :
+					list.addLast();
+					break;
+				case 3 :
+					list.deletFirst();
+					break;
+				case 6 :
+					list.printll();
+					break;
+				default :
+					System.out.println("Invalid Choice");
+		 
+			}
+			System.out.println("Want to perform more operations ?"+ans);
+			ans = (char)br.read();	
+		}
+	}
+}
---
+++ DSA/LL/DeletLast.java
@@ -0,0 +1,134 @@
+import java.io.*;
+class LL{
+	Node  head;
+	class Node{
+		String data;
+		Node next;
+
+		public Node(String data){
+			this.data = data;
+			this.next = null;	
+		}
+
+	}
+
+	public Node createNode(){
+		System.out.print("Enter the data : ");
+		String data = null;
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			data= br.readLine();
+		}catch(IOException e){
+		
+		}
+		
+		Node newNode = new Node(data);
+		return newNode;
+	}
+	public void addFirst(){
+
+		Node newNode = createNode();
+		if(head  == null){
+			head = newNode;
+			return;	
+		}
+
+		newNode.next = head;
+		head = newNode;
+	
+	}
+	public void deletFirst(){
+		
+		if(head == null){
+			System.out.println("Invalid operation empty LinkList");
+			return ;
+		}else if(head.next == null){
+			head = null;
+		}else{
+			head = head.next;
+		}
+	}
+	public void deletLast(){
+	
+		if(head == null){
+			System.out.println("Invalid operation : Empty LinkList");
+			return;
+		}else if(head.next == null){
+			head = null;
+		}else{
+		
+			Node temp = head;
+			while(temp.next.next != null){
+				temp = temp.next;
+			}
+			temp.next = null;
+		}
+	
+	}
+		
+	public void addLast(){
+	
+		Node newNode = createNode();
+		if(head == null){
+			head = newNode;
+			return;
+		}
+
+		Node temp = head;
+
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		temp.next = newNode;
+	}
+
+	public void  printll(){
+		if(head == null){
+			System.out.println("Empty LL");
+			return;
+		}
+		Node temp = head;
+		while(temp.next != null){
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.print(temp.data+"\n");
+	
+	}
+	public static void main(String[] args) throws IOException{
+		LL list = new LL();
+		char ans = 'y';
+		while(ans != 'n' || ans != 'N'){
+			System.out.println("Enter your choice : \n 1.Add Node at first \n 2.Add Node at last \n 3.Delet First Node\n 4.Delet Last Node \n 6.Print Link List");
+			int choice = 0;
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			choice = Integer.parseInt(br.readLine());
+		 	switch(choice){
+	 			case 1 :
+					list.addFirst();
+					list.printll();
+					break;
+				case 2 :
+					list.addLast();
+					list.printll();
+					break;
+				case 3 :
+					list.deletFirst();
+					list.printll();
+					break;
+				case 4 :
+					list.deletLast();
+					list.printll();
+					break;
+				case 6 :
+					list.printll();
+					break;
+				default :
+					System.out.println("Invalid Choice");
+		 
+			}
+			System.out.println("Want to perform more operations ?"+ans);
+			ans = (char)br.read();	
+		}
+	}
+}
